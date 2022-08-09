2624d2b14d4f5575cb9897116b342b3ca9d9864e
Alhassan Faisal <faisalscientist@yahoo.com>
2022-08-09 00:47:57 +0000

    allow deps array to be set on useEventCallback

--- packages/material-ui-utils/src/useEventCallback.js
+++ packages/material-ui-utils/src/useEventCallback.js
@@ -5,10 +5,11 @@ import useEnhancedEffect from './useEnhancedEffect';
  * https://github.com/facebook/react/issues/14099#issuecomment-440013892
  * @param {function} fn
  */
-export default function useEventCallback(fn) {
+export default function useEventCallback(fn, deps = []) {
   const ref = React.useRef(fn);
   useEnhancedEffect(() => {
     ref.current = fn;
   });
-  return React.useCallback((...args) => (0, ref.current)(...args), []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return React.useCallback((...args) => (0, ref.current)(...args), deps);
 }
