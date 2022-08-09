d4f1d3be0a16133df7074a28bcf6d73f4f336f6d
Alhassan Faisal <faisalscientist@yahoo.com>
2022-08-09 00:47:43 +0000

    added filterOptions as dependant array to useEventCallback for setHighlightedIndex method

--- packages/material-ui/src/useAutocomplete/useAutocomplete.js
+++ packages/material-ui/src/useAutocomplete/useAutocomplete.js
@@ -315,73 +315,76 @@ export default function useAutocomplete(props) {
     setValueState(newValue);
   };
 
-  const setHighlightedIndex = useEventCallback(({ event, index, reason = 'auto' }) => {
-    highlightedIndexRef.current = index;
+  const setHighlightedIndex = useEventCallback(
+    ({ event, index, reason = 'auto' }) => {
+      highlightedIndexRef.current = index;
 
-    // does the index exist?
-    if (index === -1) {
-      inputRef.current.removeAttribute('aria-activedescendant');
-    } else {
-      inputRef.current.setAttribute('aria-activedescendant', `${id}-option-${index}`);
-    }
-
-    if (onHighlightChange) {
-      onHighlightChange(event, index === -1 ? null : filteredOptions[index], reason);
-    }
-
-    if (!listboxRef.current) {
-      return;
-    }
-
-    const prev = listboxRef.current.querySelector('[data-focus]');
-    if (prev) {
-      prev.removeAttribute('data-focus');
-      prev.classList.remove('Mui-focusVisible');
-    }
-
-    const listboxNode = listboxRef.current.parentElement.querySelector('[role="listbox"]');
-
-    // "No results"
-    if (!listboxNode) {
-      return;
-    }
-
-    if (index === -1) {
-      listboxNode.scrollTop = 0;
-      return;
-    }
-
-    const option = listboxRef.current.querySelector(`[data-option-index="${index}"]`);
-
-    if (!option) {
-      return;
-    }
-
-    option.setAttribute('data-focus', 'true');
-    if (reason === 'keyboard') {
-      option.classList.add('Mui-focusVisible');
-    }
-
-    // Scroll active descendant into view.
-    // Logic copied from https://www.w3.org/TR/wai-aria-practices/examples/listbox/js/listbox.js
-    //
-    // Consider this API instead once it has a better browser support:
-    // .scrollIntoView({ scrollMode: 'if-needed', block: 'nearest' });
-    if (listboxNode.scrollHeight > listboxNode.clientHeight && reason !== 'mouse') {
-      const element = option;
-
-      const scrollBottom = listboxNode.clientHeight + listboxNode.scrollTop;
-      const elementBottom = element.offsetTop + element.offsetHeight;
-      if (elementBottom > scrollBottom) {
-        listboxNode.scrollTop = elementBottom - listboxNode.clientHeight;
-      } else if (
-        element.offsetTop - element.offsetHeight * (groupBy ? 1.3 : 0) <
-        listboxNode.scrollTop
-      ) {
-        listboxNode.scrollTop = element.offsetTop - element.offsetHeight * (groupBy ? 1.3 : 0);
+      // does the index exist?
+      if (index === -1) {
+        inputRef.current.removeAttribute('aria-activedescendant');
+      } else {
+        inputRef.current.setAttribute('aria-activedescendant', `${id}-option-${index}`);
       }
-    }
-  });
+
+      if (onHighlightChange) {
+        onHighlightChange(event, index === -1 ? null : filteredOptions[index], reason);
+      }
+
+      if (!listboxRef.current) {
+        return;
+      }
+
+      const prev = listboxRef.current.querySelector('[data-focus]');
+      if (prev) {
+        prev.removeAttribute('data-focus');
+        prev.classList.remove('Mui-focusVisible');
+      }
+
+      const listboxNode = listboxRef.current.parentElement.querySelector('[role="listbox"]');
+
+      // "No results"
+      if (!listboxNode) {
+        return;
+      }
+
+      if (index === -1) {
+        listboxNode.scrollTop = 0;
+        return;
+      }
+
+      const option = listboxRef.current.querySelector(`[data-option-index="${index}"]`);
+
+      if (!option) {
+        return;
+      }
+
+      option.setAttribute('data-focus', 'true');
+      if (reason === 'keyboard') {
+        option.classList.add('Mui-focusVisible');
+      }
+
+      // Scroll active descendant into view.
+      // Logic copied from https://www.w3.org/TR/wai-aria-practices/examples/listbox/js/listbox.js
+      //
+      // Consider this API instead once it has a better browser support:
+      // .scrollIntoView({ scrollMode: 'if-needed', block: 'nearest' });
+      if (listboxNode.scrollHeight > listboxNode.clientHeight && reason !== 'mouse') {
+        const element = option;
+
+        const scrollBottom = listboxNode.clientHeight + listboxNode.scrollTop;
+        const elementBottom = element.offsetTop + element.offsetHeight;
+        if (elementBottom > scrollBottom) {
+          listboxNode.scrollTop = elementBottom - listboxNode.clientHeight;
+        } else if (
+          element.offsetTop - element.offsetHeight * (groupBy ? 1.3 : 0) <
+          listboxNode.scrollTop
+        ) {
+          listboxNode.scrollTop = element.offsetTop - element.offsetHeight * (groupBy ? 1.3 : 0);
+        }
+      }
+    },
+    [filterOptions],
+  );
 
   const changeHighlightedIndex = useEventCallback(
     ({ event, diff, direction = 'next', reason = 'auto' }) => {
